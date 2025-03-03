13ad8e7ea42a792e16d6865cb93ff4af21de10a7
jeremy-yoon <77828146+jeremy-yoon@users.noreply.github.com>
2025-03-03 10:10:28 +0900

    errorfix

--- api_server/routes/custom/__init__.py
+++ api_server/routes/custom/__init__.py
@@ -1,19 +1,16 @@
 from .clothes_routes import ClothesRoutes
 from .history_routes import HistoryRoutes
 from .image_routes import ImageRoutes
-from .workflow_routes import WorkflowRoutes
 
 class CustomRoutes:
     def __init__(self, prompt_server):
         self.clothes_routes = ClothesRoutes(prompt_server)
         self.history_routes = HistoryRoutes(prompt_server)
         self.image_routes = ImageRoutes(prompt_server)
-        self.workflow_routes = WorkflowRoutes(prompt_server)
 
     def get_routes(self):
         routes = []
         routes.extend(self.clothes_routes.get_routes())
         routes.extend(self.history_routes.get_routes())
         routes.extend(self.image_routes.get_routes())
-        routes.extend(self.workflow_routes.get_routes())
         return routes 
